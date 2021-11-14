6faccf137cb88df7f4ccafe3e812870a72771892
Oguzhan Dogdu <oguzd494@gmail.com>
2021-11-14 13:31:35 +0300

    Added data class and photos.

--- app/src/main/java/com/oguzdogdu/barley/MainActivity.kt
+++ app/src/main/java/com/oguzdogdu/barley/MainActivity.kt
@@ -3,11 +3,7 @@ package com.oguzdogdu.barley
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.oguzdogdu.barley.ui.theme.BarleyTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +11,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BarleyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BarleyTheme {
-        Greeting("Android")
-    }
+fun MyApp() {
+
+
 }
